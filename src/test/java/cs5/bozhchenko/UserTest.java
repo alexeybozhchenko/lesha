ad81ce4bfeaf568d389ed9793c672e0c576b9094
alexeybozhchenko <oleksii.bozhchenko@nure.ua>
2017-10-11 09:40:44 +0300

    Initial commit

---
+++ src/test/java/cs5/bozhchenko/UserTest.java
@@ -0,0 +1,34 @@
+package cs5.bozhchenko;
+
+import junit.framework.TestCase;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by motorcrue on 26.09.2017.
+ */
+public class UserTest extends TestCase {
+
+    private User user;
+    private Date dateOfBirth;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        user = new User();
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(1984, Calendar.MAY, 26);
+        dateOfBirth = calendar.getTime();
+    }
+public void testGetFullName(){
+        user.setFirstName("John");
+        user.setLastName("Doe");
+        assertEquals("Doe, John", user.getFullName());
+}
+
+public void testGetAge(){
+    final int year = 1984;
+    user.setDateOfBirth(dateOfBirth);
+    assertEquals(2017-year, user.getAge());
+}
+}
